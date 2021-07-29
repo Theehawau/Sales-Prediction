0360172aba891ccc98c53f0c600c39d1705d55e9
Toyin Hawau Olamide <hawau.olamide.th@gmail.com>
2021-07-29 14:26:01 +0100

    creating init files

--- tests/test_data_cleaner.py
+++ tests/test_data_cleaner.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
 import sys, os
-sys.path.append(os.path.abspath(os.path.join('../scripts')))
+sys.path.append(os.path.abspath(os.path.join('../scripts/')))
 
 from data_cleaner import Clean_Data
 utils = Clean_Data()
@@ -9,8 +9,8 @@ utils = Clean_Data()
 
 class TestClean_Data(unittest.TestCase):
     """
-    A class for unit-testing functions in the data_cleaner.py file.
-
+    A class for unit-testing functiosns in the data_cleaner.py file.
+s
     """
     def setUp(self):
         self.df = pd.read_csv('../data/store.csv').head(500)
